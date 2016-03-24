d9c4aaf51204fdb8aba9a115aef56a0debd36fa0
AnakinSklavenwalker <anakin@gmx-topmail.de>
2016-03-24 17:03:02 +0100

    updated demo-tool

--- src/Demos/MAF_DemoTool/MainWindow.xaml.cs
+++ src/Demos/MAF_DemoTool/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿namespace ModernApplicationFramework_DemoTool
+﻿using System;
+using System.Windows.Controls;
+using ModernApplicationFramework.ViewModels;
+using Menu = ModernApplicationFramework.Controls.Menu;
+using MenuItem = ModernApplicationFramework.Controls.MenuItem;
+using ToolBar = ModernApplicationFramework.Controls.ToolBar;
+
+namespace ModernApplicationFramework_DemoTool
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
@@ -7,7 +14,28 @@
     {
         public MainWindow()
         {
+            MinHeight = 600;
+            MinWidth = 1200;
             InitializeComponent();
+            SourceInitialized += MainWindow_SourceInitialized;
+        }
+
+        private void MainWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            ((MainWindowViewModel)DataContext).ToolBarHostViewModel.AddToolBar(new ToolBar { IdentifierName = "Test" }, true, Dock.Top);
+            ((MainWindowViewModel)DataContext).ToolBarHostViewModel.AddToolBar(new ToolBar { IdentifierName = "Test1" }, true, Dock.Top);
+
+            var m = new Menu();
+
+            var mi2 = new MenuItem { Header = "Test2" };
+            mi2.Items.Add(new MenuItem { Header = "Test2" });
+
+            var mi = new MenuItem { Header = "Test" };
+            mi.Items.Add(mi2);
+
+
+            m.Items.Add(mi);
+            ((MainWindowViewModel)DataContext).MenuHostViewModel.Menu = m;
         }
     }
 }
