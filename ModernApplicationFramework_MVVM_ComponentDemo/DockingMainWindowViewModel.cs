c67d070321e6935c030012745c8f3539a1f9f4e4
AnakinSklavenwalker <Anakin@gmx-topmail.de>
2016-04-14 13:46:52 +0200

    added new demo and updated framework data

---
+++ ModernApplicationFramework_MVVM_ComponentDemo/DockingMainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using ModernApplicationFramework.Caliburn.Platform.Xaml;
+using ModernApplicationFramework.MVVM.Interfaces;
+using ModernApplicationFramework.MVVM.Views;
+
+namespace ModernApplicationFramework_MVVM_ComponentDemo
+{
+    [Export(typeof(IDockingMainWindowViewModel))]
+    public class DockingMainWindowViewModel : ModernApplicationFramework.MVVM.ViewModels.DockingMainWindowViewModel
+    {
+        static DockingMainWindowViewModel()
+        {
+            ViewLocator.AddNamespaceMapping(typeof(DockingMainWindowViewModel).Namespace,
+                typeof(DockingMainWindowView).Namespace);
+        }
+
+        protected override void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            Window.Title = "Demo-MVVM-Component-Tool";
+            StatusBar.ModeText = "Ready";
+        }
+    }
+}
