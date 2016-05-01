734f749d78d6498cdfaca823231673398540d479
AnakinSklavenwalker <Anakin@gmx-topmail.de>
2016-05-01 19:12:47 +0200

    added .net detection

--- src/Main/ForcesOfCorruptionModdingTool/App.xaml.cs
+++ src/Main/ForcesOfCorruptionModdingTool/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Threading;
+using Microsoft.Win32;
 
 namespace ForcesOfCorruptionModdingTool
 {
@@ -9,11 +10,40 @@ namespace ForcesOfCorruptionModdingTool
         {
             var ex = e.Exception;
             var exInner = ex.InnerException;
-            string msg = ex.Message + "\n\n" + ex.StackTrace + "\n\n" + "Inner Exception:\n" + exInner.Message + "\n\n"
-                         + exInner.StackTrace;
+            string msg = ex.Message + "\n\n" + ex.StackTrace + "\n\n" + "Inner Exception:\n" + exInner?.Message + "\n\n"
+                         + exInner?.StackTrace;
             MessageBox.Show(msg, "Application Halted!", MessageBoxButton.OK);
             e.Handled = true;
             Current.Shutdown();
+        }
+
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            Get45Or451FromRegistry();
+        }
+
+        private static void Get45Or451FromRegistry()
+        {
+            using (var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey("SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\"))
+            {
+                if (ndpKey?.GetValue("Release") != null)
+                {
+                    if (CheckFor46DotVersion((int) ndpKey.GetValue("Release")))
+                        return;
+                    MessageBox.Show("Required .NetFramework Version 4.6 was not found");
+                    Current.Shutdown();
+                }
+                else
+                {
+                    MessageBox.Show("Required .NetFramework Version 4.6 was not found");
+                    Current.Shutdown();
+                }
+            }
+        }
+
+        private static bool CheckFor46DotVersion(int releaseKey)
+        {
+            return releaseKey >= 393295;
         }
     }
 }
