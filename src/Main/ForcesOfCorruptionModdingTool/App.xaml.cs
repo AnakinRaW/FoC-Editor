515207bfa8ae65b0d269dc8a4113b10fa6a8cc1c
AnakinSklavenwalker <Anakin@gmx-topmail.de>
2016-05-01 19:03:42 +0200

    catch exception at start up

--- src/Main/ForcesOfCorruptionModdingTool/App.xaml.cs
+++ src/Main/ForcesOfCorruptionModdingTool/App.xaml.cs
@@ -1,6 +1,19 @@
-﻿namespace ForcesOfCorruptionModdingTool
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace ForcesOfCorruptionModdingTool
 {
     public partial class App
     {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var ex = e.Exception;
+            var exInner = ex.InnerException;
+            string msg = ex.Message + "\n\n" + ex.StackTrace + "\n\n" + "Inner Exception:\n" + exInner.Message + "\n\n"
+                         + exInner.StackTrace;
+            MessageBox.Show(msg, "Application Halted!", MessageBoxButton.OK);
+            e.Handled = true;
+            Current.Shutdown();
+        }
     }
 }
