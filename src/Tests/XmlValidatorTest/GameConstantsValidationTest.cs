f6e8f4b352feab0fb720c16848c5fb7c073eb46b
AnakinSklavenwalker <Anakin@gmx-topmail.de>
2016-05-09 16:28:04 +0200

    Added xml validation added gameconstants scheme

---
+++ src/Tests/XmlValidatorTest/GameConstantsValidationTest.cs
@@ -0,0 +1,18 @@
+﻿using ForcesOfCorruptionModdingTool.AlomoEngine.Core;
+using ForcesOfCorruptionModdingTool.AlomoEngine.XmlEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XmlValidatorTest
+{
+    [TestClass]
+    public class GameConstantsValidationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var validator = new XmlValidator(ForcesOfCorruptionModdingTool.AlomoEngine.Properties.Resources.Gameconstants.ToStream());
+
+            Assert.IsTrue(validator.Validate(@"C:\Test\Gameconstants.xml"));
+        }
+    }
+}
